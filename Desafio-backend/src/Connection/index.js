98b6658449661ede03a48bd69559e8ca9583e71e
German <germaneg17@hotmail.com>
2026-04-12 21:48:56 +0100

    Corrigindo config docker

--- Desafio-backend/src/Connection/index.js
+++ Desafio-backend/src/Connection/index.js
@@ -4,11 +4,11 @@ console.log('HOST:', JSON.stringify(process.env.DB_HOST))
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
+        user: process.env.DB_USER || "postgres",
+        host: process.env.DB_HOST || "db",
+        database: process.env.DB_DATABASE || "meu_banco",
+        password: process.env.DB_PASSWORD || "postgres",
+        port: process.env.DB_PORT || 5432,
         ssl: { rejectUnauthorized: false },
     },
 })
